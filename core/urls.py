1ec7693c0e110baed059af28df8208ed2b105558
Reena-drones <kamrareena6@gmail.com>
2021-11-26 02:55:09 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('calculator.urls')),
-    path('plot/', include('plot.urls')),
+    path('', include('calculator.urls'))
 ]
